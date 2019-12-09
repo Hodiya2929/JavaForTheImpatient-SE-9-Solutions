baf6a3eb1bfa257332fff7ad23e399c394daba41
Imanuel <hodiya.eyal@gmail.com>
2019-12-09 08:14:07 +0200

    Excercise 01 - Generic stack<E>

---
+++ chapter06/Ex01GenericStack.java
@@ -0,0 +1,58 @@
+package chapter06;
+
+import java.util.ArrayList;
+
+public class  Ex01GenericStack<E> {
+	
+	private ArrayList<E> stack;
+	
+	
+	public Ex01GenericStack(){
+		stack = new ArrayList<>();
+	}
+	
+	public boolean push(E element) {
+		
+	return	stack.add(element);
+	}
+	
+	public E pop () {
+		
+		return stack.remove( stack.size()-1 );		//better is to java an 'int' size field.
+	}
+	
+	public boolean isEmpty() {
+		return stack.isEmpty();
+	}
+	
+	public static void main(String[] args) {
+		
+		var stringStack = new Ex01GenericStack<String>();
+		stringStack.push("a");
+		stringStack.push("b");
+		stringStack.push("c");
+		stringStack.push("d");
+		System.out.println(stringStack.isEmpty());
+		System.out.println(stringStack.pop());
+		System.out.println(stringStack.pop());
+		System.out.println(stringStack.pop());
+		System.out.println(stringStack.pop());
+		System.out.println(stringStack.pop());
+		System.out.println(stringStack.isEmpty());
+		
+		var integerStack = new Ex01GenericStack<Integer>();
+		integerStack.push(1);
+		integerStack.push(2);
+		integerStack.push(3);
+		integerStack.push(4);
+		System.out.println(integerStack.isEmpty());
+		System.out.println(integerStack.pop());
+		System.out.println(integerStack.pop());
+		System.out.println(integerStack.pop());
+		System.out.println(integerStack.pop());
+		System.out.println(integerStack.isEmpty());
+		
+		
+	}
+
+}
