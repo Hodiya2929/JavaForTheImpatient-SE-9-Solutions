6310fefda80ad178802e459ca304e7bd25ffa58c
Imanuel Eyal <hodiya.eyal@gmail.com>
2019-12-31 08:46:09 +0200

    Exercise 08 - changing the type bound to match inheritance case

--- chapter06/Ex08ComparablePair.java
+++ chapter06/Ex08ComparablePair.java
@@ -1,6 +1,6 @@
 package chapter06;
 
-public class Ex08ComparablePair<E extends Comparable<E> > {
+public class Ex08ComparablePair<E extends Comparable> {
 	//ensures that E is a subtype of Comparable
 
 		private E first;
@@ -41,6 +41,21 @@ public class Ex08ComparablePair<E extends Comparable<E> > {
 		public String toString() {
 			return "first=" + first + ", second=" + second;
 		}
+		
+		public static class A implements Comparable<A>{
+
+			@Override
+			public int compareTo(A o) {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+			
+		}
+		
+		public static class B extends A{
+			
+		}
+		
 
 		public static void main(String[] args) {
 			
@@ -48,6 +63,16 @@ public class Ex08ComparablePair<E extends Comparable<E> > {
 			System.out.println("Min: " + stringPair.getMin());
 			System.out.println("Max: " + stringPair.getMax()); 
 			
-		
+			/*
+			 * Why the type bound must be: "E extends Comparable <? super E>>" ? 
+			 * Here is an example: let's have a class A implementing Comparable A 
+			 * and a sub-type class B inheriting from A (and of course implementing 
+			 * Comparable<A> too. But this is the problem - B class implementing
+			 * Comparable<A> and 
+			 */
+		B b = new B();
+		//System.out.println((b instanceof Comparable));
+			
+			var ex = new Ex08ComparablePair<B>(b,b);
 		}
 	}
---
+++ chapter06/Ex09FirstLast.java
@@ -0,0 +1,22 @@
+package chapter06;
+
+import java.util.ArrayList;
+
+public class Ex09FirstLast {
+
+	public static <E> Ex07Pair<E> firstLast(ArrayList<E> arr){
+		
+		
+		
+		return null;	
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		
+
+	}
+	
+
+}
